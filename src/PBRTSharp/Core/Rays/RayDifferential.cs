742e41c80f928625ff62e909fe610f06424f4ba6
Richard Ward <ward.richard@hotmail.co.uk>
2020-04-17 23:21:32 +0100

    Added RayDifferential class

---
+++ src/PBRTSharp/Core/Rays/RayDifferential.cs
@@ -0,0 +1,28 @@
+﻿using PBRTSharp.Core.Points;
+using PBRTSharp.Core.Vectors;
+
+namespace PBRTSharp.Core.Rays
+{
+    class RayDifferential : Ray
+    {
+        public bool HasDifferentials { get; set; }
+        public Point3f RxOrigin { get; set; }
+        public Point3f RyOrigin { get; set; }
+        public Vector3f RxDirection { get; set; }
+        public Vector3f RyDirection { get; set; }
+
+        public RayDifferential(in Point3f origin, in Vector3f direction, in double tMax = double.PositiveInfinity, in double castTime = 0d) : base(origin, direction, tMax, castTime)
+        {
+            HasDifferentials = false;
+        }
+        public RayDifferential(in Ray r) : this(r.Origin, r.Direction, r.TMax, r.CastTime) { }
+
+        public void ScaleDifferentials(in double s)
+        {
+            RxOrigin = Origin + (s * (RxOrigin - Origin));
+            RyOrigin = Origin + (s * (RyOrigin - Origin));
+            RxDirection = Direction + (s * (RxDirection - Direction));
+            RyDirection = Direction + (s * (RyDirection - Direction));
+        }
+    }
+}
