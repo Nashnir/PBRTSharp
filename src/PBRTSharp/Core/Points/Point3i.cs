23928d9b2df5ad40c560c4aca8d7b5c5ae6173da
Richard Ward <ward.richard@hotmail.co.uk>
2020-04-18 23:10:42 +0100

    Changed error in double/int typing

--- src/PBRTSharp/Core/Points/Point3i.cs
+++ src/PBRTSharp/Core/Points/Point3i.cs
@@ -19,9 +19,9 @@ namespace PBRTSharp.Core.Points
         public int this[in int i] => i == 0 ? X : i == 1 ? Y : Z;
 
         // Operator overloads
-        public static Point3i operator +(in Point3i p, in Vector3f v) => new Point3i((int)(p.X + v.X), (int)(p.Y + v.Y), (int)(p.Z + v.Z));
+        public static Point3i operator +(in Point3i p, in Vector3i v) => new Point3i(p.X + v.X, p.Y + v.Y, p.Z + v.Z);
         public static Point3i operator +(in Point3i p1, in Point3i p2) => new Point3i(p1.X + p2.X, p1.Y + p2.Y, p1.Z + p2.Z);
-        public static Vector3f operator -(in Point3i p1, in Point3i p2) => new Vector3f(p1.X - p2.X, p1.Y - p2.Y, p1.Z - p2.Z);
+        public static Vector3i operator -(in Point3i p1, in Point3i p2) => new Vector3i(p1.X - p2.X, p1.Y - p2.Y, p1.Z - p2.Z);
         public static Point3i operator -(in Point3i p, in Vector3i v) => new Point3i(p.X - v.X, p.Y - v.Y, p.Z - v.Z);
         public static Point3i operator *(in double d, in Point3i p) => new Point3i((int)(d * p.X), (int)(d * p.Y), (int)(d * p.Z));
         public static explicit operator Point3f(in Point3i p) => new Point3f(p.X, p.Y, p.Z);
@@ -44,7 +44,7 @@ namespace PBRTSharp.Core.Points
         public Point3i Abs() => new Point3i(Math.Abs(X), Math.Abs(Y), Math.Abs(Z));
         public Point3i Floor() => new Point3i(X, Y, Z);
         public Point3i Ceiling() => new Point3i(X, Y, Z);
-        public Point3i Lerp(double t, in Point3i p) => ((1.0d - t) * this) + (t * p);
+        public Point3i Lerp(double t, in Point3i p) => new Point3i((int)ExtraMath.Lerp(t, X, p.X), (int)ExtraMath.Lerp(t, Y, p.Y), (int)ExtraMath.Lerp(t, Z, p.Z));
         public Point3i Permute(in int X, in int Y, in int Z) => new Point3i(this[X], this[Y], this[Z]);
     }
 }
