a8fc4e51d2a52e9748d6aca35293729c104a3e10
Richard Ward <ward.richard@hotmail.co.uk>
2020-04-18 22:07:24 +0100

    Added Bounds3f

---
+++ src/PBRTSharp/Core/Bounds/Bounds3f.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using PBRTSharp.Core.Points;
+using PBRTSharp.Core.Vectors;
+
+namespace PBRTSharp.Core.Bounds
+{
+    public readonly struct Bounds3f
+    {
+        public Point3f Min { get; }
+        public Point3f Max { get; }
+
+        public Bounds3f(Point3f min, Point3f max)
+        {
+            Min = new Point3f(Math.Min(min.X, max.X), Math.Min(min.Y, max.Y), Math.Min(min.Z, max.Z));
+            Max = new Point3f(Math.Max(min.X, max.X), Math.Min(min.Y, max.Y), Math.Min(min.Z, max.Z));
+        }
+        public Bounds3f(Point3f p)
+        {
+            Min = p;
+            Max = p;
+        }
+
+        public static Bounds3f MaximumBounds => new Bounds3f(new Point3f(double.MinValue, double.MinValue, double.MinValue),
+                                                             new Point3f(double.MaxValue, double.MaxValue, double.MaxValue));
+        public Point3f this[in int i] => i == 0 ? Min : Max;
+
+        public Point3f Corner(in int corner)
+        {
+            return new Point3f(
+                    this[corner & 1].X,
+                    this[(corner & 2) != 0 ? 1 : 0].Y,
+                    this[(corner & 4) != 0 ? 1 : 0].Z
+                );
+        }
+        public Bounds3f Union(in Point3f p)
+        {
+            return new Bounds3f(
+                    new Point3f(
+                        Math.Min(Min.X, p.X),
+                        Math.Min(Min.Y, p.Y),
+                        Math.Min(Min.Z, p.Z)
+                    ),
+                    new Point3f(
+                        Math.Max(Max.X, p.X),
+                        Math.Max(Max.Y, p.Y),
+                        Math.Max(Max.Z, p.Z)
+                    )
+                );
+        }
+        public Bounds3f Union(in Bounds3f other)
+        {
+            return new Bounds3f(
+                    new Point3f(
+                        Math.Min(Min.X, other.Min.X),
+                        Math.Min(Min.Y, other.Min.Y),
+                        Math.Min(Min.Z, other.Min.Z)
+                    ),
+                    new Point3f(
+                        Math.Max(Max.X, other.Max.X),
+                        Math.Max(Max.Y, other.Max.Y),
+                        Math.Max(Max.Z, other.Max.Z)
+                    )
+                );
+        }
+        public Bounds3f Intersect(in Bounds3f other)
+        {
+            return new Bounds3f(
+                    new Point3f(
+                        Math.Max(Min.X, other.Min.X),
+                        Math.Max(Min.Y, other.Min.Y),
+                        Math.Max(Min.Z, other.Min.Z)
+                    ),
+                    new Point3f(
+                        Math.Min(Max.X, other.Max.X),
+                        Math.Min(Max.Y, other.Max.Y),
+                        Math.Min(Max.Z, other.Max.Z)
+                    )
+                );
+        }
+        public bool Contains(in Point3f p) =>
+    p.X >= Min.X && p.X <= Max.X &&
+    p.Y >= Min.Y && p.Y <= Max.Y &&
+    p.Z >= Min.Z && p.Z <= Max.Z;
+        public bool ContainsExclusive(in Point3f p) =>
+   p.X >= Min.X && p.X < Max.X &&
+   p.Y >= Min.Y && p.Y < Max.Y &&
+   p.Z >= Min.Z && p.Z < Max.Z;
+        public Bounds3f Expand(in double delta) => new Bounds3f(Min - new Vector3f(delta, delta, delta), Max + new Vector3f(delta, delta, delta));
+        public Vector3f Diagonal() => Max - Min;
+        public double SurfaceArea()
+        {
+            var diag = Diagonal();
+            return 2 * ((diag.X * diag.Y) + (diag.X * diag.Z) + (diag.Y * diag.Z));
+        }
+        public double Volume()
+        {
+            var diag = Diagonal();
+            return diag.X * diag.Y * diag.Z;
+        }
+        public int MaximumExtent()
+        {
+            var diag = Diagonal();
+            return diag.X > diag.Y && diag.X > diag.Z ? 0 : diag.Y > diag.Z ? 1 : 2;
+        }
+        public Point3f Lerp(in Point3f t)
+        {
+            return new Point3f(
+                ExtraMath.Lerp(t.X, Min.X, Max.X),
+                ExtraMath.Lerp(t.Y, Min.Y, Max.Y),
+                ExtraMath.Lerp(t.Z, Min.Z, Max.Z)
+            );
+        }
+        public Vector3f Offset(in Point3f p)
+        {
+            var working = p - Min;
+            return new Vector3f(
+                    Max.X > Min.X ? working.X / (Max.X - Min.X) : working.X,
+                    Max.Y > Min.Y ? working.Y / (Max.Y - Min.Y) : working.Y,
+                    Max.Z > Min.Z ? working.Z / (Max.Z - Min.Z) : working.Z
+            );
+        }
+        public (Point3f centre, double radius) BoundingSphere()
+        {
+            var centre = (Min + Max) / 2;
+            return (
+                centre,
+                Contains(centre) ? centre.DistanceTo(Max) : 0
+                );
+        }
+    }
+}
